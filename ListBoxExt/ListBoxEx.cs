123052829e604613a67f6f0b58ffa52ab7c26fbe
Richard Ranft <vegassparkyrich@gmail.com>
2018-05-31 06:45:06 -0700

    Start work.

---
+++ ListBoxExt/ListBoxEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace ListBoxExt
+{
+    public class ListBoxEx : System.Windows.Forms.ListBox
+    {
+        private bool m_isDragDropCopySource = true;
+
+        [Category("Behavior (drag-and-drop)"), DefaultValue(true),
+          Description("Indicates whether ...")]
+        public bool IsDragDropCopySource
+        {
+            get { return m_isDragDropCopySource; }
+            set { m_isDragDropCopySource = value; }
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+        }
+    }
+}
